441562e2c9afe95ea8688c9f84c54c24a97bdded
Jessica Chen <jessicachen28@gmail.com>
2022-08-03 15:24:43 -0400

    Implemented Add Employee and Update Employee Role Functionality

--- index.js
+++ index.js
@@ -39,12 +39,16 @@ const promptUser = () => {
         case "Add A Role":
           addRole();
           break;
+        case "Add An Employee":
+          addEmployee();
+          break;
+        case "Update An Employee Role":
+          updateEmployee();
+          break;
         case "Exit":
           process.exit();
         default:
           promptUser();
-        // "Add An Employee",
-        // "Update An Employee Role",
       }
     });
 };
@@ -88,7 +92,6 @@ function employeeDisplay() {
       if (err) {
         return console.error(err.message);
       }
-      console.log(result);
       console.table(result);
       promptUser();
     }
@@ -136,7 +139,13 @@ Add a New Department
 
 // Function to Add a Role
 const addRole = () => {
-  db.query(`SELECT * FROM DEPARTMENT`, (err, result) => {
+  console.log(`
+===============
+Add a New Role
+===============
+`);
+
+  db.query(`SELECT name FROM DEPARTMENT`, (err, result) => {
     if (err) {
       return console.error(err.message);
     } else {
@@ -188,11 +197,158 @@ const addRole = () => {
               if (err) {
                 return console.error(err.message);
               }
-              console.log("Department Added!");
+              console.log("Role Added!");
               promptUser();
             }
           );
         });
+    }
+  });
+};
+
+// Function to Add Employee
+const addEmployee = () => {
+  console.log(`
+===================
+Add a New Employee
+===================
+`);
+
+  db.query(`SELECT title FROM role`, (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    } else {
+      const roleChoices = result.map(({ title }) => title);
+      db.query(
+        `SELECT CONCAT(first_name, ' ', last_name) as 'manager' FROM employee`,
+        (err, result) => {
+          if (err) {
+            return console.error(err.message);
+          } else {
+            const mgrChoices = result.map(({ manager }) => manager);
+            mgrChoices.unshift("None");
+
+            inquirer
+              .prompt([
+                {
+                  type: "input",
+                  name: "first_name",
+                  message: "What is the employee's first name? (Required)",
+                  validate: (firstInput) => {
+                    if (firstInput) {
+                      return true;
+                    } else {
+                      console.log("Please enter the employee's first name!");
+                      return false;
+                    }
+                  },
+                },
+                {
+                  type: "input",
+                  name: "last_name",
+                  message: "What is the employee's last name? (Required)",
+                  validate: (lastInput) => {
+                    if (lastInput) {
+                      return true;
+                    } else {
+                      console.log("Please enter the employee's last name!");
+                      return false;
+                    }
+                  },
+                },
+                {
+                  type: "list",
+                  name: "role",
+                  message: "What is the employee's role? (Required)",
+                  choices: roleChoices,
+                },
+                {
+                  type: "list",
+                  name: "manager",
+                  message: "Who is the employee's manager? (Required)",
+                  choices: mgrChoices,
+                },
+              ])
+              .then((empData) => {
+                let manager = null;
+                if (empData != "None") {
+                  manager = mgrChoices.indexOf(empData.manager);
+                }
+                db.query(
+                  `INSERT INTO employee(first_name, last_name, role_id, manager_id)
+                            VALUES ("${empData.first_name}", "${
+                    empData.last_name
+                  }", ${roleChoices.indexOf(empData.role) + 1}, ${manager})`,
+                  (err) => {
+                    if (err) {
+                      return console.error(err.message);
+                    }
+                    console.log("Employee Added!");
+                    promptUser();
+                  }
+                );
+              });
+          }
+        }
+      );
+    }
+  });
+};
+
+// Function to Update Employee Role
+const updateEmployee = () => {
+  console.log(`
+===================
+Update an Employee
+===================
+`);
+
+  db.query(`SELECT title FROM role`, (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    } else {
+      const roleChoices = result.map(({ title }) => title);
+      db.query(
+        `SELECT CONCAT(first_name, ' ', last_name) as 'employee' FROM employee`,
+        (err, result) => {
+          if (err) {
+            return console.error(err.message);
+          } else {
+            const empChoices = result.map(({ employee }) => employee);
+            inquirer
+              .prompt([
+                {
+                  type: "list",
+                  name: "employee",
+                  message:
+                    "Which employee's role do you want to update? (Required)",
+                  choices: empChoices,
+                },
+                {
+                  type: "list",
+                  name: "role",
+                  message:
+                    "Which role do you want to assign the selected employee? (Required)",
+                  choices: roleChoices,
+                },
+              ])
+              .then((empData) => {
+                db.query(
+                  `UPDATE employee SET role_id=${
+                    roleChoices.indexOf(empData.role) + 1
+                  } WHERE id=${empChoices.indexOf(empData.employee) + 1}`,
+                  (err) => {
+                    if (err) {
+                      return console.error(err.message);
+                    }
+                    console.log("Employee Updated!");
+                    promptUser();
+                  }
+                );
+              });
+          }
+        }
+      );
     }
   });
 };
